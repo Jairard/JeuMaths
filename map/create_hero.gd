56d9bfe40a37398be0a4805d88abc42bc67636cb
profmaths77 <phimac0578@orange.fr>
2019-06-26 01:21:15 +0200

    choix classe

--- map/create_hero.gd
+++ map/create_hero.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-
+enum type_class {Tank, Assassin, Mage}
+export (type_class) var class_type 
 
 
 func _ready():
@@ -14,3 +15,22 @@ func _ready():
 func _on_Button_pressed():
 	get_tree().change_scene("res://map/map 0.tscn")
 
+
+
+func _on_Tank_pressed():
+	class_type == type_class.Tank
+	print("Tank")
+#		hero = Tank
+	pass 
+
+
+func _on_Assassin_pressed():
+	class_type == type_class.Assassin
+	print("Assassin")
+	pass 
+
+
+func _on_Mage_pressed():
+	class_type == type_class.Mage
+	print("Mage")
+	pass 
