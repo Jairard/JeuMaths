ff16a384c5fd6621d73c74098fa99947339e1d21
Jairard@Windows <gaetan.champarnaud@gmail.com>
2019-07-02 22:04:16 +0200

    Add refresh_all function for hudpro

--- hud/hudpro.gd
+++ hud/hudpro.gd
@@ -2,7 +2,11 @@ extends Node2D
 
 func _ready():
 	GLOBAL.connect("refreshloot", self, "refresh_item")
-	
+	refresh_all()
+
+func refresh_all():
+	# TODO: you need to initialize your UI components here !
+	pass
 
 func refresh_item(type, value):
 	if type == GLOBAL.items.xp :
