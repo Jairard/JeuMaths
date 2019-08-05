38d5f2c6dfaff84b0427ff1ef89f3e5bd6fc9ec3
profmaths77 <phimac0578@orange.fr>
2019-08-05 17:28:45 +0200

    VRAI SAUT SA MERE

--- characters/hero.gd
+++ characters/hero.gd
@@ -9,9 +9,9 @@ enum spellType {fireball}
 # By default, out hero has no spell so its dictionary is empty
 var spells = {} # empty dictionary
 
-const gravity = 10000
+const gravity = 1300
 export(int) var vitesse = 200
-var velocite = Vector2(0,0)
+var velocite = Vector2()
 
 signal comb(valeur)
 signal spell_ready(value)
@@ -64,30 +64,39 @@ func _ready():
 #	velocite = move_and_slide(velocite,Vector2(0,-1)).normalized()
 
 func _physics_process(delta):				# 60 ticks / sec whatever fps
-	velocite = Vector2(0,0)
-	velocite.y = gravity * delta
-	velocite.x = 0
 	
-	if Input.is_action_pressed("ui_left") :									# move left
+	velocite.x = 0
+	velocite.y += gravity * delta 
+	
+	move()
+	move_and_slide(velocite,Vector2(0,-1))
+
+func move():
+	var left = Input.is_action_pressed("ui_left")
+	var right = Input.is_action_pressed("ui_right") 						# move left
+	var jump = Input.is_action_just_pressed("ui_accept")
+		
+	if left == true :
 		if !$RayCast_gauche.is_colliding(): 
 			velocite.x -= vitesse
 			$"animation hero".play("mouvement gauche")
 		else : 
 			emit_signal('comb', 0)
 
-	elif Input.is_action_pressed("ui_right") :								# move right
+	if right == true :														# move right
 		if $RayCast_droite.is_colliding() == false :
 			velocite.x += vitesse
 			$"animation hero".play("mouvement droite")
 		else : 
 			emit_signal('comb', 0)
-	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():			# jump
-		velocite.y -= 5000 
+			
+	if jump == true and is_on_floor():			# jump
+		velocite.y = - 600
 		print ("saut")
 
-	velocite = move_and_slide(velocite,Vector2(0,-1)).normalized()
-	
+#	else :
+#		velocite.x += 0
+#		$"animation hero".play("saut")
 	
 func recup_loot(value):
 	
