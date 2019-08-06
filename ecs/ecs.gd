a41312c2d57f7450a527aa5a53ca01060d9ff7ba
Jairard@Windows <gaetan.champarnaud@gmail.com>
2019-08-06 19:36:20 +0200

    Typify methods' signature + implement system & component instanciation

--- ecs/ecs.gd
+++ ecs/ecs.gd
@@ -1,102 +1,102 @@
 extends Node
 
-# Improve: inject this ?
-enum System { Damage, Spell }
-enum Component { Life, Experience }
-
 var root = null
 var active_systems = {}
 var components = {}
 
 func _ready():
-	for componentType in Component:
-		components[componentType] = {}
+	pass
 
-func set_root(node):
+func set_root(node : Node) -> void:
 	root = node
 
-func register_system(type):
-	if (active_systems.has(type)):
-		print("ECS.register_system: system " + str(type) + " is already registered")
-		return
+func register_system(systemResource : Resource) -> bool:
+	if (active_systems.has(systemResource)):
+		print("ECS.register_system: system " + systemResource.resource_path + " is already registered")
+		return false
 
-	var system = __instanciate_system(type)
+	var system = __instanciate_system(systemResource)
 	if (system == null):
-		print("ECS.register_system: couldn't create system " + str(type))
-		return
+		print("ECS.register_system: couldn't create system " + systemResource.resource_path)
+		return false
 
 	active_systems.append(system)
+	return true
 
-func unregister_system(type):
-	if (!active_systems.has(type)):
-		print("ECS.unregister_system: system " + str(type) + " is not registered")
-		return
+func unregister_system(systemResource : Resource) -> bool:
+	if (!active_systems.has(systemResource)):
+		print("ECS.unregister_system: system " + systemResource.resource_path + " is not registered")
+		return false
 
-	active_systems.erase(type)
+	active_systems[systemResource].free()
+	active_systems.erase(systemResource)
+	return true
 
-func __instanciate_system(type):
-	return null # TODO
+func __instanciate_system(systemResource : Resource):
+	return systemResource.new()
 
-func add_component(node, type):
+func add_component(node : Node, componentResource : Resource) -> bool:
 	if (node == null):
 		print("ECS.add_component: node is null")
-		return
+		return false
 
-	if (!components.has(type)):
-		print("ECS.add_component: unknown component " + str(type))
-		return
+	if (!components.has(componentResource)):
+		components[componentResource] = {}
 
 	var id = node.get_instance_id()
-	var typedComponents = components[type]
+	var typedComponents = components[componentResource]
 	if (typedComponents.has(id)):
-		print("ECS.add_component: node " + str(node) + " already has a component of type " + str(type))
-		return
+		print("ECS.add_component: node " + str(node) + " already has a component " + componentResource.resource_path)
+		return false
 
-	var component = __instanciate_component(type)
+	var component = __instanciate_component(componentResource)
 	if (component == null):
-		print("ECS.add_component: couldn't create component " + str(type))
-		return
+		print("ECS.add_component: couldn't create component " + componentResource.resource_path)
+		return false
 	
 	typedComponents[id] = component
+	return true
 
-func remove_component(node, type):
+func remove_component(node : Node, componentResource : Resource) -> bool:
 	if (node != null):
 		print("ECS.remove_component: node is null")
-		return
+		return false
 
-	if (!components.has(type)):
-		print("ECS.remove_component: unknown component " + str(type))
-		return
+	if (!components.has(componentResource)):
+		print("ECS.remove_component: unknown component " + componentResource.resource_path)
+		return false
 
 	var id = node.get_instance_id()
-	var typedComponents = components[type]
+	var typedComponents = components[componentResource]
 	if (!typedComponents.has(id)):
-		print("ECS.remove_component: node " + str(node) + " doesn't have a component of type " + str(type))
-		return
+		print("ECS.remove_component: node " + str(node) + " doesn't have a component " + componentResource.resource_path)
+		return false
 
+	typedComponents[id].free()
 	typedComponents.erase(id)
+	return true
 
-func __get_component(id, type):
-	if (!components.has(type)):
+func __get_component(id : int, componentResource : Resource):
+	if (!components.has(componentResource)):
 		return null
 
-	var typedComponents = components[type]
+	var typedComponents = components[componentResource]
 	if (!typedComponents.has(id)):
 		return null
 
 	return typedComponents[id]
 
-func __instanciate_component(type):
-	return null # TODO
+func __instanciate_component(componentResource : Resource):
+	return componentResource.new()
 
-func _process(dt):
+func _process(dt : float) -> void:
 	if (root == null):
 		return
 
 	for system in active_systems:
 		__process_system(system, root, dt)
 
-func __process_system(system, node, dt):
+func __process_system(system, node : Node, dt):
 	if (node == null):
 		return
 
@@ -109,16 +109,16 @@ func __process_system(system, node, dt):
 		__process_system(system, child, dt)
 
 # Gets all the components that the system needs
-func __get_components_for_system(system, id):
+func __get_components_for_system(system, id : int):
 	var components = {}
 	for componentType in system.get_used_components():
 		if (components.has(componentType)):
-			print("ECS.__get_components_for_system: system " + system + " uses several times the component of type " + str(componentType))
+			print("ECS.__get_components_for_system: system " + system.resource_path + " uses several times the component of type " + componentType.resource_path)
 			return null
 
 		var component = __get_component(id, componentType)
 		if (component == null):
-			print("ECS.__get_components_for_system: object " + str(id) + " doesn't have a component of type " + str(componentType))
+			print("ECS.__get_components_for_system: object " + str(id) + " doesn't have a component of type " + componentType.resource_path)
 			return null
 
 		components[componentType] = component
