a55c82816f0ea741ad5054bd8e6d98527a49ccae
Jairard@Windows <gaetan.champarnaud@gmail.com>
2019-08-06 21:23:39 +0200

    Add Example of a component and a system

---
+++ ecs/Components/Library.gd
@@ -0,0 +1,5 @@
+extends Node
+
+class_name ComponentsLibrary
+
+const Position = preload("res://ecs/Components/Position.gd")
---
+++ ecs/Systems/Library.gd
@@ -0,0 +1,5 @@
+extends Node
+
+class_name SystemsLibrary
+
+const MoveRight = preload("res://ecs/Systems/MoveRight.gd")
