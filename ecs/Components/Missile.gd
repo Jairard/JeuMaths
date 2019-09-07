9077b9b5336ec9a8c2eda985906960f67c06993b
Jairard@Windows <gaetan.champarnaud@gmail.com>
2019-09-07 14:45:15 +0200

    Update MissileComponent so that it stores the target id

--- ecs/Components/Missile.gd
+++ ecs/Components/Missile.gd
@@ -2,7 +2,7 @@ extends Component
 
 class_name MissileComponent
 
-var missile = true
+var target_id : int = -1
 #var target :  Node2D
 #var target_pos  :  Vector2
 
@@ -12,9 +12,16 @@ var missile = true
 #
 #func init_position () :
 #	target_pos  = target.position
+
+func init(target : Node) -> void:
+	if (target != null):
+		set_target_id(target.get_instance_id())
 	
-func get_missile() -> bool :
-	return missile
+func get_target_id() -> int :
+	return target_id
 	
-func set_missile(value : bool) -> void :
-	value = missile
+func get_target() -> Node :
+	return instance_from_id(target_id) as Node
+
+func set_target_id(id : int) -> void :
+	target_id = id
