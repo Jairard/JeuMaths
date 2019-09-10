8b3eee261c014a13aa08744735e8dbeb77ca166b
Jairard@Windows <gaetan.champarnaud@gmail.com>
2019-09-11 00:05:38 +0200

    More Array utils !

--- Utils/Array_Utils.gd
+++ Utils/Array_Utils.gd
@@ -7,3 +7,26 @@ func remove_IFP(array : Array, elem : Object) -> bool:
 		array.remove(idx)
 
 	return found
+
+func contains(array: Array, elem : Object) -> bool:
+	var idx = array.find(elem)
+	return (idx != -1)
+
+# Return true is all the elements of `src` are also in `dst`
+func is_included_in(src: Array, dst : Array) -> bool:
+	for elem in src:
+		var idx = dst.find(elem)
+		if (idx == -1):
+			return false
+	return true
+
+# Return the elements that are in `src` but not in `dst`
+func get_diff(src : Array, dst : Array) -> Array:
+	var res : Array = []
+
+	for elem in src:
+		var idx = dst.find(elem)
+		if (idx == -1):
+			res.push_back(elem)
+
+	return res
