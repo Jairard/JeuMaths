ea9a7644a58ef9bc7fd852ed11d044e17752b097
Jairard@Windows <gaetan.champarnaud@gmail.com>
2019-10-23 12:25:58 +0200

    Add visual debug helpers in Component and an example in VelocityComponent

--- ecs/Component.gd
+++ ecs/Component.gd
@@ -12,3 +12,21 @@ func __set_object_id(newId : int) -> void:
 
 func _on_destroyed() -> void:
 	pass
+
+func draw_debug_line_to(to : Vector2, color : Color = Color.black, frameCount : int = 1) -> int:
+	return draw_debug_line(Vector2(0, 0), to, color, frameCount)
+
+func draw_debug_line(from : Vector2, to : Vector2, color : Color = Color.black, frameCount : int = 1) -> int:
+	var node : Node2D = get_node() as Node2D
+	if (node != null):
+		from = node.to_global(from)
+		to = node.to_global(to)
+	return DebugUtils.add_line(from, to, color, frameCount)
+
+func draw_debug_rect(area : Rect2, color : Color, frameCount : int = 1, filled : bool = true) -> int:
+	var node : Node2D = get_node() as Node2D
+	if (node != null):
+		area.position = node.to_global(area.position)
+		area.size *= node.get_scale()
+		area.end = area.position + area.size
+	return DebugUtils.add_rect(area, color, frameCount, filled)
--- ecs/Components/Velocity.gd
+++ ecs/Components/Velocity.gd
@@ -3,6 +3,7 @@ extends Component
 class_name VelocityComponent
 
 var vel : Vector2 = Vector2(0,0)
+var debug_line_id : int = -1
 
 func get_velocity() -> Vector2:
 	return vel
@@ -13,4 +14,8 @@ func set_velocity(_vel : Vector2) -> void:
 	if (10 < x and x < 90) or (y > 10 and y < 90) : 
 		print ("set_velocity : " + str(vel) + " -> " + str(_vel))
 	vel = _vel
-	
+	DebugUtils.remove_shape(debug_line_id)
+	debug_line_id = draw_debug_line_to(vel, Color.cyan, -1)
+
+func _on_destroyed() -> void:
+	DebugUtils.remove_shape(debug_line_id)
