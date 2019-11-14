1634a28198689f76e2b3473998d397c438ed6e83
Jairard@Windows <gaetan.champarnaud@gmail.com>
2019-11-14 15:39:09 +0100

    Avoid error when getting navigation node

--- ecs/Systems/Missile.gd
+++ ecs/Systems/Missile.gd
@@ -23,7 +23,10 @@ func _process_node(dt : float, components : Dictionary) -> void:
 		DebugUtils.add_line(target_pos, missile_pos, Color.orange,1)
 		
 		# Compute path and move
-		var node_navigation = misl_comp.get_node().get_parent().get_node("Navigation2D")
+		var parent = misl_comp.get_node().get_parent()
+		var node_navigation = null
+		if (parent.has_node("Navigation2D")):
+			node_navigation = parent.get_node("Navigation2D")
 		var dir_to = MoveUtils.get_direction_to(target_pos, missile_pos, node_navigation)
 		DebugUtils.add_line(missile_pos, missile_pos + dir_to * speed, Color.blue,1)
 		var dir = target_pos - missile_pos														
