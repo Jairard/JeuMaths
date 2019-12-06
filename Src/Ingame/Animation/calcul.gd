ad4f44c5b9ba8e69da4606d30ca76c7fa7814f3a
profmaths77 <phimac0578@orange.fr>
2019-12-06 14:29:31 +0100

    load questions from file
    we don't use an array or questions - on the script - anymore

--- Src/Ingame/Animation/calcul.gd
+++ Src/Ingame/Animation/calcul.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var count = 0
 var answer_listeners  : Array = []
-var load_calcul1 = load_c("res://Assets/Questions/calcul1.json")
+var load_calcul1 = load_c("res://Assets/Questions/questions.json")
 
 func set_answer_listener(listeners : Array) -> void:
 	answer_listeners  = listeners
@@ -10,15 +10,18 @@ func set_answer_listener(listeners : Array) -> void:
 func load_c(path : String) -> Dictionary:
 	var file = File.new()
 	file.open(path, File.READ)
-	print (file.get_as_text())
-	var text = "{\"question\" : \"1 + 1\", \"answers\" : [{\"text\" : \"1\",\"is_good_answer\" : false},{\"text\" : \"2\",\"is_good_answer\" : true}] }"
+	var text = file.get_as_text()
+#	print (file.get_as_text())
+#	var text = "{\"question\" : \"1 + 1\", \"answers\" : [{\"text\" : \"1\",\"is_good_answer\" : false},{\"text\" : \"2\",\"is_good_answer\" : true}] }"
 	var dict = parse_json(text)
 	file.close()
 	return dict
 	
-func setup_question(dict : Dictionary) -> void:
-	$question/MarginContainer/calcul.text = dict["question"]
-	var answers : Array = dict["answers"]
+func setup_question(dict : Array) -> void:
+	print (dict[0]["questions"][0]["question1"])
+
+	$question/MarginContainer/calcul.text = dict[0]["questions"][0]["question1"]
+	var answers : Array = dict[0]["questions"][0]["answers1"]
 	var ans_position : Vector2 = Vector2(0,100) 
 	for ans in answers:
 		var button : Button = Button.new() 
@@ -43,7 +46,6 @@ func _process(delta):
 func on_answer_pressed(is_good_answer : bool):
 	var answer = AnswerListenerComponent.answer.false
 	if is_good_answer == true:
-		print ("true")
 		answer = AnswerListenerComponent.answer.true
 	for listener in answer_listeners:
 		var component : AnswerListenerComponent = listener as AnswerListenerComponent
