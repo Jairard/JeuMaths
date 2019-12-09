b58998fbe1000936b58600fb5999a93ae9d1c66f
profmaths77 <phimac0578@orange.fr>
2019-12-09 13:06:07 +0100

    create question button on script

--- Src/Ingame/Animation/calcul.gd
+++ Src/Ingame/Animation/calcul.gd
@@ -4,6 +4,8 @@ var count = 0
 var answer_listeners  : Array = []
 var load_calcul = load_c("res://Assets/Questions/questions.json")
 
+var button_question : Button = Button.new()
+
 func set_answer_listener(listeners : Array) -> void:
 	answer_listeners  = listeners
 	
@@ -16,19 +18,22 @@ func load_c(path : String) -> Dictionary:
 	return dict
 	
 func setup_question(dict : Array) -> void:
+#	var button_question : Button = Button.new()		  
+	add_child(button_question)
 	randomize()
 	var rq = randi() % 2
 	var ra = randi() % 10
-	$question/MarginContainer/calcul.text = dict[rq]["questions"][ra]["question"]
+#	$question/MarginContainer/calcul.text = dict[rq]["questions"][ra]["question"]
+	button_question.text = dict[rq]["questions"][ra]["question"]
 	var answers : Array = dict[rq]["questions"][ra]["answers"]
 	var ans_position : Vector2 = Vector2(0,100) 
 	for ans in answers:
-		var button : Button = Button.new() 
-		button.text = ans["text"]
+		var button_answer : Button = Button.new() 
+		button_answer.text = ans["text"]
 		ans_position.x += 100
-		button.set_position(ans_position)
-		add_child(button)
-		button.connect("pressed", self, "on_answer_pressed", [ans["is_good_answer"]])
+		button_answer.set_position(ans_position)
+		add_child(button_answer)
+		button_answer.connect("pressed", self, "on_answer_pressed", [ans["is_good_answer"]])
 		
 
 func _ready():
@@ -49,7 +54,7 @@ func on_answer_pressed(is_good_answer : bool):
 	for listener in answer_listeners:
 		var component : AnswerListenerComponent = listener as AnswerListenerComponent
 		if component != null:
-			component.set_answer(answer)		
+			component.set_answer(answer)			
 	setup_question(load_calcul)
 
 
