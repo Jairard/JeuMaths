f8827765eebcd17e3ace2d0ddd1144372ed3765d
profmaths77 <phimac0578@orange.fr>
2019-12-16 00:02:41 +0100

    random questions

--- Src/Ingame/Animation/calcul.gd
+++ Src/Ingame/Animation/calcul.gd
@@ -4,10 +4,11 @@ var count = 0
 var answer_listeners  : Array = []
 var load_calcul = load_c("res://Assets/Questions/questions.json")
 
-var button_question : Button = Button.new()
-
 func set_answer_listener(listeners : Array) -> void:
 	answer_listeners  = listeners
+	
+func get_answer_listener() -> Array:
+	return answer_listeners
 	
 func load_c(path : String) -> Dictionary:
 	var file = File.new()
@@ -17,15 +18,18 @@ func load_c(path : String) -> Dictionary:
 	file.close()
 	return dict
 	
-func setup_question(dict : Array) -> void:
-#	var button_question : Button = Button.new()		  
+func setup_question(dict : Array) -> void:	 
+	var button_question : Button = Button.new() 
 	add_child(button_question)
-	randomize()
-	var rq = randi() % 2
-	var ra = randi() % 10
-#	$question/MarginContainer/calcul.text = dict[rq]["questions"][ra]["question"]
-	button_question.text = dict[rq]["questions"][ra]["question"]
-	var answers : Array = dict[rq]["questions"][ra]["answers"]
+	
+	var random_theme 	: int 			= RandomUtils.randi_to(len(dict))						
+	var questions		: Array 		= dict[random_theme]["questions"]
+	var random_question : int 			= RandomUtils.randi_to(len(questions))
+	var chosen_question : Dictionary 	= questions[random_question]
+	
+	button_question.text = chosen_question["question"]
+	var answers 		: Array = chosen_question["answers"]
+	
 	var ans_position : Vector2 = Vector2(0,100) 
 	for ans in answers:
 		var button_answer : Button = Button.new() 
@@ -33,6 +37,10 @@ func setup_question(dict : Array) -> void:
 		ans_position.x += 100
 		button_answer.set_position(ans_position)
 		add_child(button_answer)
+		if ans["is_good_answer"] == true:
+			var style : StyleBoxFlat = StyleBoxFlat.new()
+			style.set_bg_color(Color(0,255,0))
+			button_answer.add_stylebox_override("hover",style)
 		button_answer.connect("pressed", self, "on_answer_pressed", [ans["is_good_answer"]])
 		
 
@@ -55,6 +63,5 @@ func on_answer_pressed(is_good_answer : bool):
 		var component : AnswerListenerComponent = listener as AnswerListenerComponent
 		if component != null:
 			component.set_answer(answer)			
-	setup_question(load_calcul)
 
 
