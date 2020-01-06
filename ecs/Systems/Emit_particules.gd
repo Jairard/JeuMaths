2756e3b9680dccb387d3523a961f9dcd1ee1eae7
profmaths77 <phimac0578@orange.fr>
2020-01-06 21:43:30 +0100

    emit particules at the end of the fight

--- ecs/Systems/Emit_particules.gd
+++ ecs/Systems/Emit_particules.gd
@@ -6,11 +6,14 @@ func _get_mandatory_components() -> Array:
 	return [ComponentsLibrary.EmitPArticules]
 
 func _get_system_dependencies() -> Array:
-	return [SystemsLibrary.Move]
+	return [SystemsLibrary.Endfight]
 
 func _process_node(dt : float, components : Dictionary) -> void:
 	var emit_comp		 = 	components[ComponentsLibrary.EmitPArticules] 	 	 as  EmitParticulesComponent
-	
+	print ("emit comp : ", emit_comp.emit)
 	if emit_comp.get_emit() == true:
+		print ("2")
 		var test_emit = emit_comp.get_node()
-		test_emit.get_node("Particles2D").emitting = true
+		if emit_comp.get_node("Particules2D") != null:
+			print ("3")
+			test_emit.get_node("Particles2D").emitting = true
--- ecs/Systems/End_fight.gd
+++ ecs/Systems/End_fight.gd
@@ -3,15 +3,17 @@ extends System
 class_name EndfightSystem
 
 func _get_mandatory_components() -> Array:
-	return [ComponentsLibrary.Health, ComponentsLibrary.EndFight]
+	return [ComponentsLibrary.Health, ComponentsLibrary.EndFight, ComponentsLibrary.EmitPArticules]
 	
 func _process_node(dt : float, components : Dictionary) -> void:
 	
-	var comp_health 		= components[ComponentsLibrary.Health] 		as HealthComponent
-	var comp_end_fight 		= components[ComponentsLibrary.EndFight] 	as EndfightComponent
-	
+	var comp_health 		= components[ComponentsLibrary.Health] 					as HealthComponent
+	var comp_end_fight 		= components[ComponentsLibrary.EndFight] 				as EndfightComponent
+	var emit_comp		 	= 	components[ComponentsLibrary.EmitPArticules] 	 	as  EmitParticulesComponent
+
 	var current_health = comp_health.get_health()
 	if current_health <= 0:
-		print ("node : ", comp_end_fight.get_node())
+#		print ("node : ", comp_end_fight.get_node())
+		emit_comp.emit == true
 		comp_end_fight.set_end(true)
 
