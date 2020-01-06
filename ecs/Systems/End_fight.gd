ee008850672b74881ed5a8919cafa78be4eda76f
profmaths77 <phimac0578@orange.fr>
2020-01-07 00:08:02 +0100

    Change scene after fight

--- ecs/Systems/End_fight.gd
+++ ecs/Systems/End_fight.gd
@@ -3,17 +3,17 @@ extends System
 class_name EndfightSystem
 
 func _get_mandatory_components() -> Array:
-	return [ComponentsLibrary.Health, ComponentsLibrary.EndFight, ComponentsLibrary.EmitPArticules]
+	return [ComponentsLibrary.Health, ComponentsLibrary.EmitPArticules]
 	
 func _process_node(dt : float, components : Dictionary) -> void:
 	
 	var comp_health 		= components[ComponentsLibrary.Health] 					as HealthComponent
-	var comp_end_fight 		= components[ComponentsLibrary.EndFight] 				as EndfightComponent
 	var emit_comp		 	= 	components[ComponentsLibrary.EmitPArticules] 	 	as  EmitParticulesComponent
 
 	var current_health = comp_health.get_health()
 	if current_health <= 0:
-#		print ("node : ", comp_end_fight.get_node())
+		var new_scene = FileBankUtils.loaded_scenes["map_fire"]
+		var node = comp_health.get_node()
+		node.get_tree().change_scene(new_scene)
 		emit_comp.emit == true
-		comp_end_fight.set_end(true)
 
