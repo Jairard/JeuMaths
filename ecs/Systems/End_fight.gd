e02a09e91861f6f87c74eb93440dfbad0b45fb8d
profmaths77 <phimac0578@orange.fr>
2020-01-11 01:52:29 +0100

    timer at the end of the fight

--- ecs/Systems/End_fight.gd
+++ ecs/Systems/End_fight.gd
@@ -12,9 +12,13 @@ func _process_node(dt : float, components : Dictionary) -> void:
 
 	var current_health = comp_health.get_health()
 	if current_health <= 0:
-		var new_scene = FileBankUtils.loaded_scenes["map_fire"]
+		var new_scene = FileBankUtils.loaded_scenes["rewards"]
 		var node = comp_health.get_node()
 		emit_comp.set_emit(true)
-#		node.get_tree().change_scene(new_scene)
+#		var t = Timer.new()
+#		t.set_wait_time(3)
+#		t.start()
+		yield(node.get_parent().get_tree().create_timer(3.0), "timeout")
+		node.get_tree().change_scene(new_scene)
 		
 
