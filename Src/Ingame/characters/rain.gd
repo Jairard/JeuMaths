f918157d2333f528ee18a0db32de6bf76d511e2f
profmaths77 <phimac0578@orange.fr>
2020-01-26 14:37:10 +0100

    clear code

--- Src/Ingame/characters/rain.gd
+++ Src/Ingame/characters/rain.gd
@@ -1,25 +1,8 @@
 extends KinematicBody2D
 
-#enum type_rain  {
-#	health_bonus = 0
-#	health_malus = 1
-#	coin = 2
-#	}
-#
-#export (type_rain) var rain_type
-
 
 func _process(delta):
 	if self.position.y >= 540 :
 		queue_free()
 
-#func start(pos, type):
-#	self.position = pos
-#	rain_type = type
-#	if rain_type == 0 :
-#		$Sprite.modulate = Color (0,0,1)
-#	if rain_type == 1 :
-#		$Sprite.modulate = Color (1,0,0)
-#	if rain_type == 2 :
-#		$Sprite.modulate = Color (255,215,0)
 
