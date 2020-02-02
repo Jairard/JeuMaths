a2860993ce45430be4830a1fc64d5eb2b49ab0ea
profmaths77 <phimac0578@orange.fr>
2020-02-02 23:17:42 +0100

    test save stats

--- Src/Outgame/Signin.gd
+++ Src/Outgame/Signin.gd
@@ -1,18 +1,41 @@
 extends Node2D
 
+var stats : Array = []
 var dict = {}
 var file = File.new()
+var init_stats : Dictionary = {
+							"pseudo" : "",
+							"stats" : [
+										{
+										"damage":15,"health":70,"level":0,
+										"xp":0,"treasure":0,"good_answer":1,
+										"wrong_answer":1,"boss_killed":1
+										}
+									  ]
+						}
 
 func _ready():
-	_load()
+	pass
 
 #func _process(delta):
 #	pass
 
+func if_pseudo():
+	for i in len(stats):
+		print (stats[i]["pseudo"])
+		if stats[i]["pseudo"] == $TileMap/pseudo.get_text():
+			print ("load")
+			_load()
+		else :
+			print ("save")
+			stats.append(init_stats)
+			save()
+
 func _on_Button_pressed():
 	
-	dict["pseudo"] = $TileMap/pseudo.get_text()
-	save()
+	var pseudo : String = $TileMap/pseudo.get_text()
+	if_pseudo()
+
 	get_tree().change_scene("res://Src/Outgame/create_hero.tscn")
 
 func _load():
@@ -23,7 +46,10 @@ func _load():
 		
 func save():
 	
-	file.open("res://log_in/pseudo.json", File.WRITE)
-	var text = to_json(dict)
+	file.open("res://Assets/Stats_Characters/Hero_Stats.json", File.WRITE)
+	var text = to_json(init_stats)
 	file.store_string(text)
 	file.close()
+	
+	
+	
