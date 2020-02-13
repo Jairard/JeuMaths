0b469e291417491548d26830bde17579895c69e0
profmaths77 <phimac0578@orange.fr>
2020-02-13 23:14:06 +0100

    fix shop map
    add hero node to hierarchy

--- Src/Outgame/Map_Shop.gd
+++ Src/Outgame/Map_Shop.gd
@@ -3,14 +3,21 @@ extends Node2D
 onready var treasure_damage	= 	preload("res://Src/Ingame/characters/gold.tscn")
 onready var treasure_health	= 	preload("res://Src/Ingame/characters/gold.tscn")
 onready var treasure_life	= 	preload("res://Src/Ingame/characters/gold.tscn")
-
 onready var hero 			= 	preload("res://Src/Ingame/characters/hero.tscn")
-onready var heroNode = hero.instance()
-onready var health_comp_hero = ECS.add_component(heroNode, ComponentsLibrary.Health, TagsLibrary.Tag_Hero) as HealthComponent
-onready var damage_comp_hero = ECS.add_component(heroNode, ComponentsLibrary.Damage, TagsLibrary.Tag_Hero) as DamageComponent
-onready var treasure_comp_hero = ECS.add_component(heroNode, ComponentsLibrary.Treasure, TagsLibrary.Tag_Hero) as TreasureComponent
+
+var heroNode : Node2D = null
+var health_comp_hero : Component = null
+var damage_comp_hero : Component = null
+var treasure_comp_hero : Component = null
 
 func _ready():
+	heroNode = hero.instance()
+	heroNode.deactivate()
+	add_child(heroNode)
+	health_comp_hero = ECS.add_component(heroNode, ComponentsLibrary.Health, TagsLibrary.Tag_Hero) as HealthComponent
+	damage_comp_hero = ECS.add_component(heroNode, ComponentsLibrary.Damage, TagsLibrary.Tag_Hero) as DamageComponent
+	treasure_comp_hero = ECS.add_component(heroNode, ComponentsLibrary.Treasure, TagsLibrary.Tag_Hero) as TreasureComponent
+
 	var treasureNode_damage = treasure_damage.instance()
 	add_child(treasureNode_damage)
 	treasureNode_damage.apply_scale(Vector2(1.8,1.8))
@@ -31,6 +38,9 @@ func _ready():
 
 	var treasure_life_pos_comp = ECS.add_component(treasureNode_life, ComponentsLibrary.Position) as PositionComponent
 	treasure_life_pos_comp.set_position(Vector2(810,218))
+#	treasureNode_life.
+	
+	ECS.clear_ghosts()
 	
 func _on_Damages_pressed():
 
@@ -40,7 +50,7 @@ func _on_Damages_pressed():
 
 
 func _on_Health_pressed():
-
+	
 	health_comp_hero.set_health(health_comp_hero.get_health() + 10)
 	treasure_comp_hero.set_treasure(treasure_comp_hero.get_treasure() - 10)
 	get_tree().change_scene("res://Src/Outgame/Stats.tscn")
@@ -51,3 +61,6 @@ func _on_regen_health_pressed():
 	treasure_comp_hero.set_treasure(treasure_comp_hero.get_treasure() - 20)
 	get_tree().change_scene("res://Src/Outgame/Stats.tscn")
 
+	
+	
+	
--- ecs/Components/Score_global_counter.gd
+++ ecs/Components/Score_global_counter.gd
@@ -6,26 +6,30 @@ var good_answer 	: int = 0
 var wrong_answer 	: int = 0
 var boss_killed 	: int = 0
 
-func init_score() -> float:
+func init(_good_answer : int, _wrong_answer : int,
+		  _boss_killed : int):
+	if (!was_ghost()):
+		good_answer = _good_answer
+		wrong_answer = _wrong_answer
+		boss_killed = _boss_killed
+		
+func compute_score() -> float:
 	return ((get_boss_killed()+1) * (float(get_good_answer()+1) / (get_wrong_answer()+1)) * 1000)
 
 func get_good_answer() -> int:
 	return good_answer
 
 func set_good_answer(_good_answer) -> void:
-	if (!was_ghost()):
 		good_answer = _good_answer
 
 func get_wrong_answer() -> int:
 	return wrong_answer
 
 func set_wrong_answer(_wrong_answer) -> void:
-	if (!was_ghost()):
 		wrong_answer = _wrong_answer
 
 func get_boss_killed() -> int:
 	return boss_killed
 
 func set_boss_killed(_boss_killed) -> void:
-	if (!was_ghost()):
 		boss_killed = _boss_killed
