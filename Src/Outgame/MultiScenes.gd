5b36fb9055f272806e296cb874671288e286f0a7
profmaths77 <phimac0578@orange.fr>
2020-02-21 01:09:42 +0100

    graphics changes on scenes

--- Src/Outgame/MultiScenes.gd
+++ Src/Outgame/MultiScenes.gd
@@ -1,51 +1,46 @@
 extends Node2D
 
-func _ready():
-	pass
-
-func _on_Start_pressed():
-	get_tree().change_scene("res://Src/Outgame/Start.tscn")
-
 func _on_Sign_In_pressed():
-	get_tree().change_scene("res://Src/Outgame/Signin.tscn")
+	get_tree().change_scene(FileBankUtils.loaded_scenes["sign_in"])
 
 func _on_create_hero_pressed():
-	get_tree().change_scene("res://Src/Outgame/create_hero.tscn")
+	get_tree().change_scene(FileBankUtils.loaded_scenes["create_hero"])
 
-func _on_Log_In_pressed():
-	get_tree().change_scene("res://Src/Outgame/Login.tscn")
 
 func _on_map_level_pressed():
-	get_tree().change_scene("res://Src/Outgame/map_fire.tscn")
+	get_tree().change_scene(FileBankUtils.loaded_scenes["playing_map"][1]["map_fire"])
 
 func _on_Map_fight_1_pressed():
-	get_tree().change_scene("res://Src/Outgame/map_fight_1.tscn")
+	get_tree().change_scene(FileBankUtils.loaded_scenes["map_fight_1"])
 
 func _on_Map_fight_2_pressed():
-	get_tree().change_scene("res://Src/Outgame/map_fight_2.tscn")
+	get_tree().change_scene(FileBankUtils.loaded_scenes["map_fight_2"])
 
 
 func _on_Shop_pressed():
-	get_tree().change_scene("res://Src/Outgame/Map_Shop.tscn")
+	get_tree().change_scene(FileBankUtils.loaded_scenes["shop"])
 
 
 func _on_Map_water_pressed():
-	get_tree().change_scene("res://Src/Outgame/Map_Water.tscn")
-
-
+	get_tree().change_scene(FileBankUtils.loaded_scenes["map_water"])
+	
 
 func _on_Rewards_pressed():
-	get_tree().change_scene("res://Src/Outgame/Rewards.tscn")
+	get_tree().change_scene(FileBankUtils.loaded_scenes["rewards"])
 
 
 func _on_Stats_pressed():
-	get_tree().change_scene("res://Src/Outgame/Stats.tscn")
+	get_tree().change_scene(FileBankUtils.loaded_scenes["stats"])
 
 
 func _on_Death_pressed():
-	get_tree().change_scene("res://Src/Outgame/Death.tscn")
+	get_tree().change_scene(FileBankUtils.loaded_scenes["death"])
 
 
 func _on_Next_level_pressed():
-	get_tree().change_scene("res://Src/Outgame/Next_level.tscn")
+	get_tree().change_scene(FileBankUtils.loaded_scenes["next_level"])
 
+
+
+func _on_Map_tuto_pressed():
+	get_tree().change_scene(FileBankUtils.loaded_scenes["playing_map"][0]["map_tuto"])
