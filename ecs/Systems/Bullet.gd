beae7142e4a6426587beb160516367a8623234a6
profmaths77 <phimac0578@orange.fr>
2020-02-28 23:38:02 +0100

    delete line() on moveutils

--- ecs/Systems/Bullet.gd
+++ ecs/Systems/Bullet.gd
@@ -8,10 +8,9 @@ func _get_mandatory_components() -> Array:
 func _process_node(dt : float, components : Dictionary) -> void:
 	var pos_comp = components[ComponentsLibrary.Position] as PositionComponent
 	var vect = pos_comp.get_position()
-	if MoveUtils.line(vect) >= 0:
-		vect.x -= 400 * dt
-		pos_comp.set_position(vect)
+	
+	vect.x -= 400 * dt
+	pos_comp.set_position(vect)
 
-	else :
-		pos_comp.get_node().queue_free()
+	
 
