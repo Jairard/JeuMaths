912cddc708fa172eca4e2ecc2b8210fdfe99585b
profmaths77 <phimac0578@orange.fr>
2020-03-04 01:02:43 +0100

    New scene for transition between scenes
    anime a transition and its reverse

---
+++ Src/Outgame/Scene_changer.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+func change_scene(path):
+	$Control.show()
+	$AnimationPlayer.play("scene_changer")
+	yield($AnimationPlayer, "animation_finished")
+	assert(get_tree().change_scene(path) == OK)
+	$AnimationPlayer.play_backwards("scene_changer")
+	yield($AnimationPlayer, "animation_finished")
+	$Control.show()
+	
