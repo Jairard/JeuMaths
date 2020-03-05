1577cb3dc6ef9f1b0d2af7110d1ca6269fc1b6bf
profmaths77 <phimac0578@orange.fr>
2020-03-05 10:56:16 +0100

    Tween Utils

---
+++ Utils/TweenAnimationUtils.gd
@@ -0,0 +1,43 @@
+extends Node
+
+func tween_hero_collision(node_hero : Node2D) -> void:
+	var node = node_hero.get_node("hero_spr")
+	var tween = node.get_node("Tween")
+
+	tween.interpolate_property(node, "modulate", Color("#ffffff"), Color("#00ffffff"),0.2, 
+	Tween.TRANS_BACK,Tween.EASE_IN_OUT)
+	tween.start()
+
+	tween.interpolate_property(node, "modulate", Color("#00ffffff"),Color("#ffffff"),0.2, 
+	Tween.TRANS_BACK,Tween.EASE_IN_OUT)
+	tween.start()
+	yield(tween, "tween_completed")
+	
+	tween.interpolate_property(node, "modulate", Color("#ffffff"), Color("#00ffffff"),0.2, 
+	Tween.TRANS_BACK,Tween.EASE_IN_OUT)
+	tween.start()
+
+	tween.interpolate_property(node, "modulate", Color("#00ffffff"),Color("#ffffff"),0.2, 
+	Tween.TRANS_BACK,Tween.EASE_IN_OUT)
+	tween.start()
+	yield(tween, "tween_completed")
+
+func tween_hero_loot(node_loot : Node2D) -> void:
+	var node = node_loot.get_node("Sprite")
+	var tween = node.get_node("Tween")
+	tween.interpolate_property(
+	node, "position", node.position,
+	Vector2(node.position.x, node.position.y - 500),
+	1.5, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
+	
+	tween.interpolate_property(
+	node, "scale", Vector2(1,1), Vector2(2, 2),
+	1.5, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
+
+	tween.interpolate_property(
+	node, "modulate", Color(1.0,1.0,1.0,1.0), Color(1.0,1.0,1.0,0.0),
+	0.5, Tween.TRANS_BACK, Tween.EASE_IN_OUT,1)
+	
+	tween.start()
+	yield(tween, "tween_completed")
+
