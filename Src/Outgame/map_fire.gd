f08ad99fce25af389d82d3fe68da0811d9f307d6
profmaths77 <phimac0578@orange.fr>
2020-03-06 14:18:26 +0100

    Test checkpoint on map_fire
    get health and position
    => tween on colorRect to animate a transition effect

--- Src/Outgame/map_fire.gd
+++ Src/Outgame/map_fire.gd
@@ -14,7 +14,11 @@ onready var health			= 	preload("res://Src/Ingame/characters/Health.tscn")
 onready var score			= 	preload("res://Assets/Textures/hud/hud_score.tscn")
 
 var health_comp_hero : Component = null
+var pos_comp : Component = null
 var heroNode = null
+
+export var color_game = Color("#00000000")
+export var color_tumble = Color("#000000")
 
 var file = File.new()
 var dict = {}
@@ -62,8 +66,8 @@ func load_characters() :
 	ECS.add_component(heroNode, ComponentsLibrary.Bounce)
 
 
-	var pos_comp = ECS.add_component(heroNode, ComponentsLibrary.Position) as PositionComponent
-	pos_comp.set_position(Vector2(23000,500))
+	pos_comp = ECS.add_component(heroNode, ComponentsLibrary.Position) as PositionComponent
+	pos_comp.set_position(Vector2(350,500))
 	ECS.add_component(heroNode, ComponentsLibrary.Collision)
 	ECS.add_component(heroNode, ComponentsLibrary.Velocity)
 	var gravity_comp = ECS.add_component(heroNode, ComponentsLibrary.Gravity) as GravityComponent
@@ -135,8 +139,32 @@ func _on_Timer_timeout():
 
 func _process(delta):
 	if health_comp_hero.get_health() <= 0:
-		Scene_changer.change_scene(FileBankUtils.loaded_scenes["death"])
+		if pos_comp.get_position().x <= 11500:
+			Scene_changer.change_scene(FileBankUtils.loaded_scenes["death"])
+		if pos_comp.get_position().x >11500 and pos_comp.get_position().x <= 20000:
+			tween(Vector2(11500, 500))
+			health_comp_hero.set_health(health_comp_hero.get_health_max())			
+		if pos_comp.get_position().x >20000:
+			tween(Vector2(20000, 500))
+			health_comp_hero.set_health(health_comp_hero.get_health_max())
+		
+
+func tween(pos : Vector2):
+		var tween = Tween.new()
+		add_child(tween)
+		$CanvasModulate.show()
+		tween.interpolate_property($CanvasModulate, "color", color_tumble, color_game, 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+		tween.start()
+		yield(tween, "tween_completed")
+		remove_child(tween)
+		
+		$Control/ColorRect.show()
+		$Control/AnimationPlayer.play("test")
+		yield($Control/AnimationPlayer, "animation_finished")
 	
+		pos_comp.set_position(pos)
+		$Control/ColorRect.hide()
+		$CanvasModulate.hide()
 
 func _load_monsters():
 	EntitiesUtils.create_monster(self, monster, Vector2(750, 295), gold, health, damage)
@@ -159,6 +187,7 @@ func _load_bullets():
 	EntitiesUtils.create_bullet(self, spawn_fire, Vector2(4600,550))
 	EntitiesUtils.create_bullet(self, spawn_fire, Vector2(6150,550))
 	EntitiesUtils.create_bullet(self, spawn_fire, Vector2(7400,550))
+	EntitiesUtils.create_bullet(self, spawn_fire, Vector2(13000,770))	
 	EntitiesUtils.create_bullet(self, spawn_fire, Vector2(15500,770))
 	EntitiesUtils.create_bullet(self, spawn_fire, Vector2(17000,770))
 	EntitiesUtils.create_bullet(self, spawn_fire, Vector2(21000,770))
