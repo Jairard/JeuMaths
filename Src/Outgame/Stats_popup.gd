393ef02912f5022aa8ff328b52c1402615a0d52c
profmaths77 <phimac0578@orange.fr>
2020-03-10 13:16:39 +0100

    rename variable

--- Src/Outgame/Stats_popup.gd
+++ Src/Outgame/Stats_popup.gd
@@ -10,7 +10,7 @@ func _ready():
 	heroNode.deactivate()
 	add_child(heroNode)
 	var score_comp : Component = ECS.add_component(heroNode, ComponentsLibrary.Scoreglobal) as ScoreglobalcounterComponent
-	score_comp.init(FileBankUtils.good_answer, FileBankUtils.wrong_answer, FileBankUtils.victories, FileBankUtils.defeats)
+	score_comp.init_stats(FileBankUtils.good_answer, FileBankUtils.wrong_answer, FileBankUtils.victories, FileBankUtils.defeats)
 	var hud_comp : Component = ECS.add_component(heroNode, ComponentsLibrary.Hud) as HudComponent
 	hud_comp.init_stats(get_good_answer(), get_wrong_answer(), get_victories(),get_defeats())
 	
