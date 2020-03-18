41b803f9fcaa9fcbc900115c881602618ff64943
profmaths77 <phimac0578@orange.fr>
2020-03-18 15:56:29 +0100

    tryin to make an Util to swap to the right scene

--- Utils/FileBankUtils.gd
+++ Utils/FileBankUtils.gd
@@ -55,6 +55,13 @@ func _notification(what):
 			loaded_heroes_stats = {}
 		loaded_heroes_stats[pseudo] = stats
 		save_json(loaded_heroes_stats, stats_File_Name)
-		
-		
-		
+
+func load_right_scene():
+	if FileBankUtils.scene_counter % 4 == 1:
+		return Scene_changer.change_scene(FileBankUtils.loaded_scenes["playing_map"][1]["map_fire"])
+	elif FileBankUtils.scene_counter % 4 == 2: 
+		return Scene_changer.change_scene(FileBankUtils.loaded_scenes["map_water"])
+	elif FileBankUtils.scene_counter % 4 == 3: 
+		return Scene_changer.change_scene(FileBankUtils.loaded_scenes["playing_map"][2]["map_fire_0"])
+	elif FileBankUtils.scene_counter % 4 == 4: 
+		return Scene_changer.change_scene(FileBankUtils.loaded_scenes["upside_down"])
