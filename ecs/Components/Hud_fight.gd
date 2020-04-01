354cae8faf64762805ca2f1dba0e57a106faf3f3
profmaths77 <phimac0578@orange.fr>
2020-04-01 19:01:51 +0200

    health bar update

--- ecs/Components/Hud_fight.gd
+++ ecs/Components/Hud_fight.gd
@@ -8,7 +8,7 @@ var _damage			: 	Label = null
 var original_health : int = 0
 var target_health : int = 0
 
-var health_anim_time : float = 5
+var health_anim_time : float = 1
 var health_current_anim_time : float = 0
 
 
@@ -40,15 +40,17 @@ func update_displayed_health(dt : float) -> void:
 	if original_health != target_health:
 		health_current_anim_time = min(health_anim_time, health_current_anim_time + dt)
 		_health_value.value = lerp(original_health, target_health, health_current_anim_time/health_anim_time)
+		print (_health_value.value)
 		if health_current_anim_time == health_anim_time :
 			original_health = target_health
-		
+			health_current_anim_time = 0
+
 
 func set_health(health : int) -> void :
 	if health != target_health:
 #		print ("health : ", health, "  /  target : ", target_health)		
 		target_health = health
-		health_current_anim_time = 0
+#		health_current_anim_time = 0
 	
 	if _health_value.value > 0:
 		_health_label.text = "%s / %s" % [health,  _health_value.max_value]	
