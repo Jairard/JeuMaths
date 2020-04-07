da6c44c6d4460cf3fdac351cb4ba6d4211adb987
profmaths77 <phimac0578@orange.fr>
2020-04-07 17:01:30 +0200

    Import fractions

--- Tools/test.py
+++ Tools/test.py
@@ -10,7 +10,7 @@ def render_latex(formula, fontsize=12, dpi=300, format_='png'):
     plt.close(fig)
     return buffer_.getvalue()
 
-for i in range(-12, 12, 1):
+for i in range(-20, 20, 1):
     for j in range(-20, 20, 1):
         raw_numerator = float(i)
         raw_denominator = float(j)
@@ -27,7 +27,7 @@ for i in range(-12, 12, 1):
         image_bytes = render_latex(formula, fontsize=10, dpi=300, format_='png')
         with open(output_file_name, 'wb') as image_file:
             image_file.write(image_bytes)
-    print("-12 / 12         Done !")
+    print("-20 / 20         Done !")
 
 for i in range(-200, 200, 10):
     for j in range(-200, 200, 10 ):
@@ -47,22 +47,3 @@ for i in range(-200, 200, 10):
         with open(output_file_name, 'wb') as image_file:
             image_file.write(image_bytes)
     print("-200 / 200       Done !")
-
-for i in range(1, 20, 1):
-    for j in range(1, 20, 1 ):
-        raw_numerator = float(i)
-        raw_denominator = float(j)
-
-        float_fmt = "%." + "f"
-        numerator = float_fmt % raw_numerator
-        denominator = float_fmt % raw_denominator
-
-        formula = r"\frac{%s}{%s}" % (numerator, denominator)
-        output_file_name = "frac_%s_%s.png" % (raw_numerator, raw_denominator)
-
-        print("Saving fraction %s/%s to '%s'..." % (numerator, denominator, output_file_name))
-
-        image_bytes = render_latex(formula, fontsize=10, dpi=300, format_='png')
-        with open(output_file_name, 'wb') as image_file:
-            image_file.write(image_bytes)
-    print("1 / 20       Done !")
