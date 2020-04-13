65ca9e4f2007ed848e03cdb670c0769b02a1e842
profmaths77 <phimac0578@orange.fr>
2020-04-13 12:51:06 +0200

    Trying to design new hud score (gold + score)

--- Assets/Textures/hud/hud_score.gd
+++ Assets/Textures/hud/hud_score.gd
@@ -11,15 +11,15 @@ func set_hero_node(node : Node2D) -> void:
 	heroNode = node
 
 func _ready():
-	$CanvasLayer/KinematicBody2D/AnimationPlayer.play("anim_gold")
+	$CanvasLayer/HBoxContainer/gold/Label2/AnimationPlayer.play("anim_gold")
 	$CanvasLayer.add_child(shop)
 	$CanvasLayer.add_child(stats)	
 
 func get_score() -> RichTextLabel:
-	return $CanvasLayer/Score										as RichTextLabel
+	return $CanvasLayer/HBoxContainer/score/RichTextLabel			as RichTextLabel
 
 func get_treasure() -> Label :
-	return $CanvasLayer/HBoxContainer2/MarginContainer/treasure 	as Label
+	return $CanvasLayer/HBoxContainer/gold/Label					as Label
 
 func _on_Button_pressed():
 	if shop.is_shown():
