14be0fe68d921c2ef193aed3f69adbbf3c38defe
profmaths77 <phimac0578@orange.fr>
2020-04-13 13:05:19 +0200

    Update hbox and label on hud_score

--- Assets/Textures/hud/hud_score.gd
+++ Assets/Textures/hud/hud_score.gd
@@ -11,15 +11,15 @@ func set_hero_node(node : Node2D) -> void:
 	heroNode = node
 
 func _ready():
-	$CanvasLayer/HBoxContainer/gold/Label2/AnimationPlayer.play("anim_gold")
+	$CanvasLayer/HBoxContainer/Gold/gold_sprite/AnimationPlayer.play("anim_gold")
 	$CanvasLayer.add_child(shop)
 	$CanvasLayer.add_child(stats)	
 
 func get_score() -> RichTextLabel:
-	return $CanvasLayer/HBoxContainer/score/RichTextLabel			as RichTextLabel
+	return $CanvasLayer/HBoxContainer/Score/score_richText/RichTextLabel			as RichTextLabel
 
 func get_treasure() -> Label :
-	return $CanvasLayer/HBoxContainer/gold/Label					as Label
+	return $CanvasLayer/HBoxContainer/Gold/gold_label					as Label
 
 func _on_Button_pressed():
 	if shop.is_shown():
