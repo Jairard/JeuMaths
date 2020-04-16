e679f269f2e25281b752a57ecfa7e08fecbc5aed
profmaths77 <phimac0578@orange.fr>
2020-04-16 11:02:35 +0200

    New Fade scene on autoload
    the easiest code
    a good visual

--- Utils/Fade.gd
+++ Utils/Fade.gd
@@ -1,8 +1,11 @@
 extends CanvasLayer
 
-func change_scene(path):
+func change_scene(path, delay = 0.5):
+	yield(get_tree().create_timer(delay), "timeout")
+	$ColorRect.show()
 	$AnimationPlayer.play("fade")
 	yield($AnimationPlayer, "animation_finished")
 	assert(get_tree().change_scene(path) == OK)
 	$AnimationPlayer.play_backwards("fade")
 	yield($AnimationPlayer, "animation_finished")
+	$ColorRect.hide()
