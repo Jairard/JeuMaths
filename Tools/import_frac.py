99f5e0df239cbc5302232a22e5ca3b1bc0c9167d
profmaths77 <phimac0578@orange.fr>
2020-04-22 21:30:34 +0200

    Create import_frac.py

---
+++ Tools/import_frac.py
@@ -0,0 +1,14 @@
+﻿import json
+
+dict= []
+frac = []
+chain = ".png"
+
+file = open("Godot_Fractions.txt", "r")
+dict = file.read()
+file.close
+frac = json.loads(dict)
+##print (dict)
+
+print (frac)
+
