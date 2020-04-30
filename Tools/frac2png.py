69444a0b4014597153d2f43ad3f6a93e182c989e
Jairard@Windows <gaetan.champarnaud@gmail.com>
2020-04-30 09:57:41 +0200

    Add padding for exponent formula

--- Tools/frac2png.py
+++ Tools/frac2png.py
@@ -3,20 +3,20 @@ import io
 import sys
 import os
 
-def render_latex(formula, fontsize=12, dpi=300, format_='png'):
+def render_latex(formula, fontsize=12, dpi=300, format_='png', pad_inches=0.0):
     fig = plt.figure(figsize=(0.01, 0.01))
     fig.text(0, 0, u'${}$'.format(formula), fontsize=fontsize)
     buffer_ = io.BytesIO()
-    fig.savefig(buffer_, dpi=dpi, transparent=True, format=format_, bbox_inches='tight', pad_inches=0.0)
+    fig.savefig(buffer_, dpi=dpi, transparent=True, format=format_, bbox_inches='tight', pad_inches=pad_inches)
     plt.close(fig)
     return buffer_.getvalue()
 
-def generate_formula(formula, file_name, dst_folder = ".", verbose = True):
+def generate_formula(formula, file_name, dst_folder = ".", verbose = True, pad_inches=0.0):
     file_path = os.path.join(dst_folder, file_name)
     if (verbose):
         print("Saving formula %s to '%s'..." % (formula, file_path))
 
-    image_bytes = render_latex(formula, fontsize=10, dpi=300, format_='png')
+    image_bytes = render_latex(formula, fontsize=10, dpi=300, format_='png', pad_inches=pad_inches)
     with open(file_path, 'wb') as image_file:
         image_file.write(image_bytes)
         if (verbose):
@@ -31,7 +31,7 @@ def generate_fraction(numerator, denominator, file_name, dst_folder = ".", verbo
 
 def generate_exponent(number, exponent, file_name, dst_folder = ".", verbose = True):
     formula = r"{%d}^{%d}" % (number, exponent)
-    generate_formula(formula, file_name, dst_folder, verbose)
+    generate_formula(formula, file_name, dst_folder, verbose, pad_inches=0.025)
 
 if __name__ == '__main__':
     numerator = int(sys.argv[1])
