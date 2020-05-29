e6d83b2c2d3f775e91a62ac91793e457fd01bd4d
profmaths77 <phimac0578@orange.fr>
2020-05-29 18:09:40 +0200

    Improve Grid Tool with Color

--- Src/Outgame/Grid.gd
+++ Src/Outgame/Grid.gd
@@ -1,10 +1,12 @@
 tool
 extends Node2D
 
-export var line_H_lengh  = 1920	setget set_line_H_lengh
-export var line_V_lengh  = 1920	setget set_line_V_lengh
-export var line_width  = 20	setget set_line_width
-export var start  = Vector2(0,0)	setget set_start
+export var line_H_lengh : int = 1920	setget set_line_H_lengh
+export var line_V_lengh : int = 1920	setget set_line_V_lengh
+export var line_width : int = 20	setget set_line_width
+export var start : Vector2 = Vector2(0,0)	setget set_start
+export var color_H : Color = Color.white	setget set_color_H
+export var color_V : Color = Color.greenyellow	setget set_color_V
 const origin : Vector2 = Vector2(0,0)
 
 func _init() -> void:
@@ -26,19 +28,23 @@ func set_start(pos : Vector2) -> void:
 	start = pos
 	update()
 
+func set_color_H(value : Color) -> void:
+	color_H = value
+	update()
+
+func set_color_V(value : Color) -> void:
+	color_V = value
+	update()
+
 func _draw():
-#	var line_H_lengh : int = 1920
-#	var line_V_lengh : int = 1920
-#	var line_width : int = 20
-#	var start : Vector2 = Vector2(0,0)
 	for i in range(0, line_H_lengh, 160):
-		draw_line(start, Vector2((start.x + line_V_lengh), start.y),  Color.white, line_width)
+		draw_line(start, Vector2((start.x + line_V_lengh), start.y),  color_H, line_width)
 		start.y += 160
 
 	start = origin
 
 	for j in range(0, line_V_lengh, 160):
-		draw_line(start,  Vector2(start.x, start.y + line_V_lengh),  Color.greenyellow,  line_width)
+		draw_line(start,  Vector2(start.x, start.y + line_V_lengh),  color_V,  line_width)
 		start.x += 160
 
 	start = origin
