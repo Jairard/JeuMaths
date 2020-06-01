2b4c2c6d8273f6de343c970b7d17c4c909cbdd9d
profmaths77 <phimac0578@orange.fr>
2020-06-01 11:57:32 +0200

    Improve grid tools

--- Src/Outgame/Grid.gd
+++ Src/Outgame/Grid.gd
@@ -3,6 +3,10 @@ extends Node2D
 
 export var line_H_lengh : int = 1920	setget set_line_H_lengh
 export var line_V_lengh : int = 1920	setget set_line_V_lengh
+export var line_H_spacing : int = 160	setget set_line_H_spacing
+export var line_V_spacing : int = 160	setget set_line_V_spacing
+export var line_H_count : int = 0
+export var line_V_count : int = 0
 export var line_width : int = 20	setget set_line_width
 export var start : Vector2 = Vector2(0,0)	setget set_start
 export var color_H : Color = Color.white	setget set_color_H
@@ -18,6 +22,14 @@ func set_line_H_lengh(value : int) -> void:
 
 func set_line_V_lengh(value : int) -> void:
 	line_V_lengh = value
+	update()
+
+func set_line_H_spacing(value : int) -> void:
+	line_H_spacing = value
+	update()
+
+func set_line_V_spacing(value : int) -> void:
+	line_V_spacing = value
 	update()
 
 func set_line_width(value : int) -> void:
@@ -37,15 +49,15 @@ func set_color_V(value : Color) -> void:
 	update()
 
 func _draw():
-	for i in range(0, line_H_lengh, 160):
-		draw_line(start, Vector2((start.x + line_V_lengh), start.y),  color_H, line_width)
-		start.y += 160
+	for i in range(0, line_H_count):#, line_V_spacing):
+		draw_line(start, Vector2((start.x + line_H_lengh), start.y),  color_H, line_width)
+		start.y += line_V_spacing
 
 	start = origin
 
-	for j in range(0, line_V_lengh, 160):
+	for j in range(0, line_V_count):#, line_H_spacing):
 		draw_line(start,  Vector2(start.x, start.y + line_V_lengh),  color_V,  line_width)
-		start.x += 160
+		start.x += line_H_spacing
 
 	start = origin
 
