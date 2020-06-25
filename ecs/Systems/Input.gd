0cda0854a4b73e3c81dc056dd5e4fcdb351bc6df
Gatgat <gaetan.champarnaud@gmail.com>
2020-06-25 22:36:32 +0200

    Factorize InputSystem code to make it clearer

--- ecs/Systems/Input.gd
+++ ecs/Systems/Input.gd
@@ -8,29 +8,46 @@ func _get_mandatory_components() -> Array:
 func _process_node(dt : float, components : Dictionary) -> void:
 	var comp = components[ComponentsLibrary.Movement] as MovementComponent
 	var input = components[ComponentsLibrary.InputListener] as InputListenerComponent
-	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left") :
-		comp.set_direction(comp.dir.none)
-	elif Input.is_action_pressed("ui_right"):# or Input.is_action_pressed("Arrow_right"):
-		comp.set_direction(comp.dir.right)
-	elif Input.is_action_pressed("ui_left"):# or Input.is_action_pressed("Arrow_left"):
-		comp.set_direction(comp.dir.left)
-	else :
-		comp.set_direction(comp.dir.none)
 
+	# Move direction
+	var move_direction = MoveUtils.dir.none
+	if (input.get_move()):
+		move_direction = MoveUtils.dir.mouse
+	else:
+		var keyboard_dir = MoveUtils.dir.none
+		if Input.is_action_pressed("ui_right") != Input.is_action_pressed("ui_left") :
+			if Input.is_action_pressed("ui_right"):# or Input.is_action_pressed("Arrow_right"):
+				keyboard_dir = MoveUtils.dir.right
+			elif Input.is_action_pressed("ui_left"):# or Input.is_action_pressed("Arrow_left"):
+				keyboard_dir = MoveUtils.dir.left
+
+		var touch_dir = input.get_move_dir()
+		move_direction = __get_move_direction(keyboard_dir, touch_dir)
+	comp.set_direction(move_direction)
+
+	# Jump
 	if Input.is_action_just_pressed("ui_accept"):
 		input.set_is_jumping(true)
 
-	if input.get_left():
-		comp.set_direction(comp.dir.left)
-	if input.get_right():
-		comp.set_direction(comp.dir.right)
 	if input.is_jumping():
 		# Consume the input
 		input.set_is_jumping(false)
 		# Trigger a real jump if possible
 		if comp.get_node().is_on_floor():
 			comp.set_is_jumping(true)
-	
-	if input.get_move():
-		comp.set_direction(comp.dir.mouse)
-#		input.set_move(false)
+
+func __get_move_direction(keyboard_dir, touch_dir):
+	# Both inputs are the same, no conflict
+	if (keyboard_dir == touch_dir):
+		return keyboard_dir
+
+	# keyboard_dir is unset, we use touch_dir
+	if (keyboard_dir == MoveUtils.dir.none):
+		return touch_dir
+	# touch_dir is unset, we use keyboard_dir
+	if (touch_dir == MoveUtils.dir.none):
+		return keyboard_dir
+
+	# Both keyboard_dir & touch_dir are set to different directions.
+	# This should not happen but anyway we consider that the direction cancel each other.
+	return MoveUtils.dir.none
