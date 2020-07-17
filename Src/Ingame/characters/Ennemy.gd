e72213c42aa62a54b70d228b2b73ea4acfedd04e
profmaths77 <phimac0578@orange.fr>
2020-07-17 07:59:49 +0200

    New texture + animation for Enemy Scene

--- Src/Ingame/characters/Ennemy.gd
+++ Src/Ingame/characters/Ennemy.gd
@@ -1,32 +1,4 @@
 extends KinematicBody2D
 
-signal spell_enemy_ready(value)
-
-onready var lot   = preload("res://Src/Ingame/Animation/loot_monstre.tscn")
-
-enum spellType {ballfire}
-var spells = {}
-
-func death(valeur):
-	var pos = self.position
-	var i = lot.instance()
-	i.start(pos)
-#	if boo :
-	get_parent().add_child(i)
-
-	queue_free()
-
-
-func add_spell(spellRoot, type):
-	spells[type] = spellRoot
-
-func cast_spell(type):
-	if (spells.has(type)):
-		emit_signal("spell_enemy_ready", 1)
-	else:
-		print("cast_spell: no spell registered for type " + str(type))
-
-
-#func _process(delta):
-#	pass
-
+func _ready():
+	$AnimationPlayer.play("anim")
