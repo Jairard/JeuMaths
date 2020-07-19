ae6a57f25d1a6bf93c343ed3b24491c84074f8bd
profmaths77 <phimac0578@orange.fr>
2020-07-19 10:51:33 +0200

    Restet health when death

--- Src/Outgame/Death.gd
+++ Src/Outgame/Death.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _ready():
-	pass
+	FileBankUtils.health = FileBankUtils.health_max
 
 func _on_reset_button_pressed():
 	Fade.change_scene(FileBankUtils.loaded_scenes["map_fight_1"])
