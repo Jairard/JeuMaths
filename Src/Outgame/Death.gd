0c4b81aea719e2133d50e2be4fd1de14f1a2b2d3
profmaths77 <phimac0578@orange.fr>
2020-07-19 11:04:19 +0200

    regen data

--- Src/Outgame/Death.gd
+++ Src/Outgame/Death.gd
@@ -1,7 +1,4 @@
 extends Node2D
 
-func _ready():
-	FileBankUtils.health = FileBankUtils.health_max
-
 func _on_reset_button_pressed():
 	Fade.change_scene(FileBankUtils.loaded_scenes["map_fight_1"])
