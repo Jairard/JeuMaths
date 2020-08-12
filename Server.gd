4f0722cd84bc5313ef33d89b130fa934921eb634
profmaths77 <phimac0578@orange.fr>
2020-08-12 22:32:27 +0200

    network test

--- Server.gd
+++ Server.gd
@@ -1,22 +1,76 @@
-extends SceneTree
+extends Node2D
 
-func _init():
-	var done = false
+var Tcp_Server : TCP_Server = TCP_Server.new()
+var connected_clients : Array = []
+var connecting_clients : Array = []
+var timer = false
 
-	var socket = PacketPeerUDP.new()
-	if(socket.listen(4242,"127.0.0.1") != OK):
-		print("An error occurred listening on port 4242")
-		done = true;
-	else:
-		print("Listening on port 4242 on localhost")
+func Server_Start_Listening():
+	print("Starting the server...")
+	var success = Tcp_Server.listen(1337,"*")
+	if success != OK:
+		print ("ERROR listening : ",success)
+		return
 
-	while(done != true):
-		if(socket.get_available_packet_count() > 0):
-			var data = socket.get_packet().get_string_from_ascii()
-			if(data == "quit"):
-				done = true
-			else:
-				print("Data received: " + data)
-	socket.close()        
-	print("Exiting application")    
-	self.quit()
+	print("The server is waiting for clients")
+
+func _ready():
+	Server_Start_Listening()
+
+func _process(delta):
+	if timer:
+		timer = false
+		if Tcp_Server.is_connection_available(): 
+			var client : StreamPeerTCP = Tcp_Server.take_connection()
+			var pseudo = client.get_var()
+			connecting_clients.append(client)
+
+			print("A client has connected ! -------- pseudo : ", str(pseudo))
+		print("There are currently ", str(len(connected_clients)) ," clients connected")
+
+
+func _exit_tree():
+	print("Closing the server")
+	Tcp_Server.stop()
+
+func _handle_client():
+	var clients_to_remove : Array = []
+	for client in connecting_clients:
+		match(client.get_status()):
+			StreamPeerTCP.STATUS_NONE:
+				clients_to_remove.append(client)
+				print("Client disconnected after trying to connect")
+			StreamPeerTCP.STATUS_CONNECTING:
+				pass
+			StreamPeerTCP.STATUS_CONNECTED:
+				clients_to_remove.append(client)
+				connected_clients.append(client)
+			StreamPeerTCP.STATUS_ERROR:
+				clients_to_remove.append(client)
+				print("An ERROR occured while trying to connect")
+
+	for client in clients_to_remove:
+		connecting_clients.erase(client)
+
+	clients_to_remove = []
+	for client in connected_clients:
+		match(client.get_status()):
+			StreamPeerTCP.STATUS_NONE:
+				clients_to_remove.append(client)
+				print("Client disconnected")
+			StreamPeerTCP.STATUS_CONNECTING:
+				print("A connected client is trying to connect what the fuck")
+			StreamPeerTCP.STATUS_CONNECTED:
+				handle_client_message(client)
+			StreamPeerTCP.STATUS_ERROR:
+				clients_to_remove.append(client)
+				print("A dark error happened the client is fucking ahahahah")
+
+	for client in clients_to_remove:
+		connected_clients.erase(client)
+
+func handle_client_message(client : StreamPeerTCP):
+	pass
+
+func _on_Timer_timeout():
+	timer = true
--- client.gd
+++ client.gd
@@ -1,9 +1,36 @@
-extends SceneTree
-func _init():
-  var socket = PacketPeerUDP.new()
-  
-  socket.set_dest_address("127.0.0.1",4242)
-  socket.put_packet("quit".to_ascii())
-	
-  print("Exiting application")    
-  self.quit()
+extends Node2D
+
+var Tcp_Client : StreamPeerTCP = StreamPeerTCP.new()
+var pseudo : String = ""
+
+func Client_Connection():
+	print("Connecting to server...")
+	Tcp_Client.connect_to_host("127.0.0.1", 1337)
+
+	if Tcp_Client.get_connected_port() != 1337:
+		print("ERROR with port 1337")
+		Tcp_Client = null
+		return
+	if !Tcp_Client.is_connected_to_host():
+		print("ERROR : Fail to connection to Server")
+		Tcp_Client = null
+		return
+
+#	Tcp_Client.put_var(pseudo)
+	print("Connected !")
+
+func _ready():
+	Client_Connection()
+
+func _exit_tree():
+	print("Closing the server")
+	Tcp_Client.disconnect_from_host()
+
+func _process(delta):
+	if Tcp_Client == null:
+		pass
+
+func _on_Button_pressed():
+	pseudo = $LineEdit.get_text()
+	print("new pseudo")
+	Tcp_Client.put_var(pseudo)
