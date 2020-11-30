f46944b9b5967b40edd152a650d44f711826f552
profmaths77 <phimac0578@orange.fr>
2020-11-30 11:30:11 +0100

    Revert "Merge branch 'master' of https://github.com/Jairard/JeuMaths"
    This reverts commit d467edbab959880b73ca4e527c93a1928c5ccb04, reversing
    changes made to 68703333ae3eaa03db8abf0f9d54708ac09003d9.

--- Server/Tests/FakeConnectionManager.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-# Listener creation and registration is done by 'MessageListenerSandbox'
-func on_network_message(type: int, message) -> void:
-	var msg : LoginRequestMessage = message as LoginRequestMessage
-	print("New login request from '", msg.PlayerID, "'")
